8bdbbff5a7b9322225ca547a8e37612bf0e374c0
Michael Riedmann <michael.riedmann@catalysts.cc>
2019-11-13 01:06:01 +0100

    for to foreach; enable travis cache

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,8 +11,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            updateItem(items[i]);
+        for (Item item : items) {
+            updateItem(item);
         }
     }
 
